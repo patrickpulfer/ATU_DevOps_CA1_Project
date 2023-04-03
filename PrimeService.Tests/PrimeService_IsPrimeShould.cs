4969a1ab1ca11f81fa6152843d4464320537760d
patrickpulfer <patrick.pulfer1@gmail.com>
2023-04-03 11:01:10 +0100

    Actions Test: Test should fail

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -9,7 +9,7 @@ namespace Prime.UnitTests.Services
         public void IsPrime_InputIs1_ReturnFalse()
         {
             var primeService = new PrimeService();
-            bool result = primeService.IsPrime(1);
+            bool result = primeService.IsPrime(2);
 
             Assert.False(result, "1 should not be prime");
         }
